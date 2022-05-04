99ed97c70d09ba644a0664a586050a747ad7e413
Bernard Lambeau <blambeau@gmail.com>
2022-05-04 18:30:04 +0200

    Bump to 0.1.0 and release.

--- lib/monolens/version.rb
+++ lib/monolens/version.rb
@@ -1,8 +1,8 @@
 module Monolens
   module Version
-    MAJOR = 2
-    MINOR = 7
-    TINY  = 1
+    MAJOR = 0
+    MINOR = 1
+    TINY  = 0
   end
   VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::TINY}"
 end
