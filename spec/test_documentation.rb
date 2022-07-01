9bc9c4f01da9569a4a82852d5b97a8308ea9a027
Bernard Lambeau <blambeau@gmail.com>
2022-07-01 17:51:46 +0200

    Fix test_documentation.rb for ruby 3.1.

--- spec/test_documentation.rb
+++ spec/test_documentation.rb
@@ -19,15 +19,24 @@ will return:
 ([^`]+)
 ```
 }
+
+def yaml_load(input)
+  if YAML.respond_to?(:unsafe_load)
+    YAML.unsafe_load(input)
+  else
+    YAML.load(input)
+  end
+end
+
 Path.dir.parent.glob('documentation/*/*.md') do |file|
   describe file do
     if file.read =~ rx
       lens, input, output = $1, $2, $3
 
       it 'has correct examples' do
-        lens = Monolens.lens(YAML.load(lens))
-        input = YAML.load(input)
-        output = YAML.load(output)
+        lens = Monolens.lens(yaml_load(lens))
+        input = yaml_load(input)
+        output = yaml_load(output)
         expect(lens.call(input)).to eql(output)
       end
     else
